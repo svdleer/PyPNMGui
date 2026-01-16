fa8a157609f2cdb25ff22bc6ec17ea029a4ed751
Silvester van der Leer <=>
2026-01-16 20:16:53 +0100

    Fix SNMP community: Z1gg0@LL (capital Z, number 1)

--- backend/config_lab.py
+++ backend/config_lab.py
@@ -12,7 +12,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccap001',
         'ip': '172.16.6.200',
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Arris',  # ccap0x = Arris E6000
         'location': 'LAB-MND-GT0002'
@@ -20,7 +20,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccap002', 
         'ip': '172.16.6.212',
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Arris',  # ccap0x = Arris E6000
         'location': 'LAB-MND-GT0002'
@@ -28,7 +28,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccap101',
         'ip': '172.16.6.201', 
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Casa',  # ccap1x = Casa 100G
         'location': 'LAB-MND-GT0002'
@@ -36,7 +36,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccap201',
         'ip': '172.16.6.202',
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Cisco',  # ccap2xx = Cisco cBR8
         'location': 'LAB-MND-GT0002'
@@ -44,7 +44,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccapv001',
         'ip': '172.16.6.160',
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Commscope',  # ccapvxxx = Commscope EVO vCCAP
         'location': 'LAB-MND-GT0002'
@@ -52,7 +52,7 @@ LAB_CMTS_SYSTEMS = [
     {
         'name': 'mnd-gt0002-ccapv002',
         'ip': '172.16.6.130',
-        'snmp_community': 'Zigg0@LL',
+        'snmp_community': 'Z1gg0@LL',
         'type': 'CCAP',
         'vendor': 'Commscope',  # ccapvxxx = Commscope EVO vCCAP
         'location': 'LAB-MND-GT0002'
